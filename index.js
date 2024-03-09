92830958c7c8c75d760e4f646c24896b9194d746
Santa <avotina@protonmail.com>
2024-03-09 23:41:10 +0200

    added functionality

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 const app = express();
+
+app.use(express.json());
 
 const port = 3000;
 
@@ -8,9 +11,21 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+
 app.get("/", (req, res) => {
     res.send("Hello from Node API");
+}); 
+
+
+app.post('/api/products', async (req, res) => {
+    try {
+      const  product = await Product.create(req.body);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }
 });
+
 
 mongoose.connect("mongodb+srv://admin:pPX9vib3SSach7Qu@backenddb.nge1dt6.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
   .then(() => {
