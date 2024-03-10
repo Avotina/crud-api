cade0c98babfc9aa9dcea43b0ae7644d0b1759e0
Santa <avotina@protonmail.com>
2024-03-10 11:47:26 +0200

    update id

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
 });
 
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
     res.send("Hello from Node API");
 }); 
 
@@ -48,6 +48,26 @@ app.post('/api/products', async (req, res) => {
     } catch (error) {
       res.status(500).json({message: error.message});
     }
+});
+
+
+// update product
+app.put('/api/product/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({message: 'Product not found'});
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
+
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  };
 });
 
 
